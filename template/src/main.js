2aca9e2691ded4ec7fb7b1abf68dbdd70537913c
yuyg <1062690261@qq.com>
2020-06-03 16:41:51 +0800

    添加http请求

--- template/src/main.js
+++ template/src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue';
 import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
 import axios from 'axios';
+import http from  '../../http/index'
 import store from './store';
 
 import './util/rem';
