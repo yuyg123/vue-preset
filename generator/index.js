1855b27fe07601b1a45bacae863632201e997c27
yuyg <1062690261@qq.com>
2020-06-03 14:48:07 +0800

    css格式规范

--- generator/index.js
+++ generator/index.js
@@ -2,6 +2,12 @@ module.exports = (api, options, rootOptions) => {
   // 复制并用 ejs 渲染 `./template` 内所有的文件
   api.render('../template')
   // 修改 `package.json` 里的字段
+  const px2rem = require('postcss-px2rem');
+  // 配置基本大小
+  const postcss = px2rem({
+    // 基准大小 baseSize，需要和rem.js中相同
+    remUnit: 16
+  });
   api.extendPackage({
     dependencies: {
       "core-js": "^3.6.5",
@@ -71,9 +77,7 @@ module.exports = (api, options, rootOptions) => {
       }
     },
     postcss: {
-      "plugins": {
-        "autoprefixer": {}
-      }
+      "plugins": [postcss]
     },
     browserslist: [
       "> 1%",
