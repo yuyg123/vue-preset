79f556a9be6dee5691fb40ca980ac6bd8174c8fc
yuyg <1062690261@qq.com>
2021-05-21 12:31:43 +0800

    更新基础模板

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = (api, options, rootOptions) => {
+  // 复制并用 ejs 渲染 `./template` 内所有的文件
+  api.render('../template')
+  // 修改 `package.json` 里的字段
+  api.extendPackage({
+    dependencies: {
+      "core-js": "^3.6.5",
+      "vue": "^2.6.11",
+      "element-ui": "^2.12.0",
+      "moment": "^2.24.0",
+      "vue-router": "^3.1.3",
+      "vuex": "^3.0.1",
+      "axios": "^0.18.0"
+    },
+    devDependencies: {
+      "@vue/cli-plugin-babel": "^4.4.0",
+      "@vue/cli-plugin-eslint": "^4.4.0",
+      "@vue/cli-service": "^4.4.0",
+      "babel-eslint": "^10.1.0",
+      "eslint": "^6.7.2",
+      "eslint-plugin-vue": "^6.2.2",
+      "vue-template-compiler": "^2.6.11",
+      "@vue/cli-plugin-vuex": "^4.0.0"
+    },
+    scripts: {
+      "serve": "vue-cli-service serve",
+      "build": "vue-cli-service build"
+    }
+  })
+}
